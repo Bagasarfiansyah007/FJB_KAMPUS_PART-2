c18dec371ff3cbd59c3b4c267ce7b430913df95c
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2021-06-08 06:48:52 +0700

    penambahn file product_detail

--- Tugas_Besar_FJB-KAMPUS_aowkawokawo/src/Model/Checkout_model.java
+++ Tugas_Besar_FJB-KAMPUS_aowkawokawo/src/Model/Checkout_model.java
@@ -10,5 +10,14 @@ package Model;
  * @author AMS
  */
 public class Checkout_model {
-       private int bagas;
+   private int no_order;
+   
+   public void setNoorder (int net_order){
+       no_order = net_order;
+   }
+   
+   public int getNoorder (){
+       return no_order;
+   }
+   
 }
